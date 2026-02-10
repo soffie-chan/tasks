a181e229ee9913b5b50fe88e94c4929afb1dcd92
Soffie <soffie@udel.edu>
2026-02-10 00:43:44 -0500

    Completed 4 reqs in task 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import img from "../src/assets/Manatee.png";
 
 function App(): React.JSX.Element {
     return (
@@ -7,7 +8,27 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript with Soffie Paul!
             </header>
-            <h1>Hello World</h1>
+            <h1>Hello World! Make way for the greatest site EVER!</h1>
+            <img src={img} alt="Manatee"></img>
+
+            <div>
+                <ul>
+                    <li>
+                        Did you know? Manatees are one of Soffie&#39;s favorite
+                        animals!
+                    </li>
+                    <li>
+                        Manatees are also called &quot;sea cows&quot; because
+                        they are large, slow-moving mammals that graze on sea
+                        grass and aquatic plants in shallow coastal waters.
+                    </li>
+                    <li>
+                        Manatees are also very gentle, friendly, and curious
+                        towards humans.
+                    </li>
+                </ul>
+            </div>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
