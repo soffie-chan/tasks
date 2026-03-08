ff133049d1342dad931b64b522c8e8a9b3951ee2
Soffie <soffie@udel.edu>
2026-03-07 22:04:34 -0500

    finished revealanswer and counter

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,19 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(false);
+
+    function revealAns(): void {
+        setVisible(!visible);
+    }
+    let button = (
+        <div>
+            <Button onClick={revealAns}>Reveal Answer</Button>
+            {visible ?
+                <div>42</div>
+            :   ""}
+        </div>
+    );
+
+    return button;
 }
