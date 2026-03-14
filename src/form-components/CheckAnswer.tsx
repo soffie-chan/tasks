3d98ea4ff4fc878f19cfaaaed1aadf2ca08ad42e
Soffie <soffie@udel.edu>
2026-03-14 18:56:22 -0400

    checkanswer works and editmode wants me dead <3

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    let correctOrNah = "";
+    const [answer, setAnswer] = useState<string>("");
+    if (answer === expectedAnswer) {
+        correctOrNah = "✔️";
+    } else {
+        correctOrNah = "❌";
+    }
+
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Answer</Form.Label>
+                <Form.Control
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setAnswer(e.target.value);
+                    }}
+                    value={answer}
+                ></Form.Control>
+            </Form.Group>
+            <div>{correctOrNah}</div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,47 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [user, setUser] = useState<string>("Your Name");
+    const [isStudent, setisStudent] = useState<boolean>(true);
+
+    const editModeOn = (
+        <div>
+            <Form.Group>
+                <Form.Label>User Name:</Form.Label>
+                <Form.Control
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setUser(e.target.value);
+                    }}
+                    value={user}
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Check
+                type="checkbox"
+                checked={isStudent}
+                className="form-check-input"
+                onChange={(e) => {
+                    setisStudent(e.target.checked);
+                }}
+            ></Form.Check>
+        </div>
+    );
+
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                checked={edit}
+                onChange={(e) => {
+                    setEdit(e.target.checked);
+                }}
+            ></Form.Check>
+            {edit ? editModeOn : <div></div>}
+            <div>
+                /{user} {isStudent ? " is a student" : " is not a student"}/
+            </div>
         </div>
     );
 }
