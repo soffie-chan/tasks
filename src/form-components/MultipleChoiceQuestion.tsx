57d15e38da63868c26b1f4477f373a2b6ee6ad7e
Soffie <soffie@udel.edu>
2026-03-14 19:22:20 -0400

    mcq works and i need dinner

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="MCQ">
+                <Form.Label>Choose The Correct Option</Form.Label>
+                <Form.Select
+                    value={choice}
+                    onChange={(e) => {
+                        setChoice(e.target.value);
+                    }}
+                >
+                    {options.map((opt: string) => (
+                        <option key={opt} value={opt}>
+                            {opt}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{choice === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
